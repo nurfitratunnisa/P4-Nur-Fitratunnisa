97608c3caf59846a62ef87071f32e691f38c2993
Nur Fitratunnisa <64566097+nurfitratunnisa@users.noreply.github.com>
2020-05-12 23:13:49 +0800

    Create Jobsheet 2.cpp
    Membuat array dua dimensi yang menampilkan NIM dari 2 mahasiswa yang berbeda

---
+++ Jobsheet 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int nim [2][2];
+nim [0][0]=0;
+nim [0][1]=8;
+nim [1][0]=0;
+nim [1][1]=9;
+
+cout << "nim pertama " << nim[0][0] << nim[0][1] << endl;
+cout << "nim kedua " << nim[1][0] << nim[1][1];
